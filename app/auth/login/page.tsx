f6e442a53af82e25a8e45ec3f3cca588e5b26d27
Aaron Jauregui Sifuentes <aaronjau21@gmail.com>
2025-11-23 23:33:39 -0500

    Login Page

---
+++ app/auth/login/page.tsx
@@ -0,0 +1,9 @@
+"use client";
+
+export default function Page() {
+  return (
+    <div className="bg-[url(/login/login.webp)] h-screen bg-cover bg-center">
+      <h1>Pagez</h1>
+    </div>
+  );
+}
